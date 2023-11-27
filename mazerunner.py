7b3706f8baa31a21bf050200155efadff79ee67a
Lindsey <151571219+westlj6@users.noreply.github.com>
2023-11-27 11:26:05 -0600

    added even more code

--- mazerunner.py
+++ mazerunner.py
@@ -54,6 +54,120 @@ ____        ____
 ...||.|\./|.||...                             
 ...||.|.X.|.||...                             
 ...||.|/.\.|.||...                             
-                             
-                             
-                           '''.strip() )
+...||.|_/...\_||...                             
+...||.......||...                             
+___|/.......\|___
+.................                             
+.................'''.strip())
+# The strip() call is used to remove the newline
+# at the start of this multiline string.
+
+CLOSED = {}
+CLOSED['A'] = wallStrToWallDict(r'''
+_____
+.....                                
+.....                                
+.....                                
+_____'''.strip()) # Paste to 6, 4.
+
+CLOSED['B'] = wallStrToWallDict(r'''
+.\.
+..\
+...
+...
+...
+../
+./.'''.strip()) # Paste to 4, 3.
+
+CLOSED['C'] = wallStrToWallDict(r'''
+___________
+...........
+...........                                
+...........
+...........
+...........
+...........
+...........                                                                                                
+...........
+___________'''.strip())# Paste to 3, 1.
+
+CLOSED['D'] = wallStrToWallDict(r'''
+./.
+/..
+...
+...
+...
+\..                                                                                                                                                                
+.\.'''.strip()) # Paste to 10, 3.
+
+CLOSED['E'] = wallStrToWallDict(r'''
+..\..
+...\_
+....|  
+....| 
+....| 
+....| 
+....| 
+....| 
+....| 
+....| 
+....| 
+.../.
+../..'''.strip()) # Paste to 0, 0.
+
+CLOSED['F'] = wallStrToWallDict(r'''
+../..
+_/...
+|....
+|....
+|....
+|....
+|....
+|....
+|....
+|....
+|....
+.\...                                                                                                                                                                                                                                                                                                                                                                
+..\..'''.strip()) # Paste to 12, 0.
+
+def displayWallDict(wallDict):
+    """Display a wall dictionary, as returned by wallStrToWallDict(), on
+    the screen."""
+    print(BLOCK * (wallDict['width'] + 2))
+    for y in range(wallDict['height']):
+        print(BLOCK, end='')
+        for x in range(wallDict['width']):
+            wall = wallDict[(x, y)]
+            if wall == '.':
+                wall = ' '
+            print(wall, end='')
+        print(BLOCK) # Print block with a newline
+    print(BLOCK * (wallDict['width'] + 2))
+
+
+def pasteWallDict(srcWallDict, dstWallDict, left, top):
+    """Copy the wall representation dictionary in srcWallDict on top of
+    the one in dstWallDict, offset to the position given by left, top."""
+    dstWallDict = copy.copy(dstWallDict)
+    for x in range(srcwallDict['width']):
+        for y in range(srcwallDict['height']):
+            dstWallDict[(x + left, y + top)] = srcWallDict[(x, y)]
+    return dstWallDict
+
+
+def makeWallDict(maze, playerx, playery, playerDirection, exitx, exity):
+    """From the player's position and direction in the maze (which has
+    an exit at exitx, exity), create the wall representation dictionary
+    by pasting wall dictionaries on top of ALL_OPEN, then return it."""
+
+    # The A-F "sections" (which are relative to the player's direction)
+    # determine which walls in the maze we check to see if we need to
+    # paste them over the wall representation dictionary we're creating.
+
+    if playerDirection == NORTH:
+        # Map of the sections, relative A
+        # to the player @:              BCD (Player facing north)
+        #                               E@F
+        offsets = (('A', 0, -2), ('B', -1, -1), ('C', 0, 1), 
+                   ('D', 1, -1), ('E', -1, 0), ('F', -1, 0))
+    
